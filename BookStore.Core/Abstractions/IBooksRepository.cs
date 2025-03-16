3337783962e8dce6998d665f11b0c668a542a6a0
VladimirAR24 <raek06@mail.ru>
2025-03-16 15:55:30 +0300

    Добавьте файлы проекта.

---
+++ BookStore.Core/Abstractions/IBooksRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookStore.CoreDomain.Models;
+
+namespace BookStore.CoreDomain.Abstractions
+{
+    public interface IBooksRepository
+    {
+        Task<Guid> Create(Book book);
+        Task<Guid> Delete(Guid id);
+        Task<List<Book>> Get();
+        Task<Guid> Update(Guid id, string title, string description, decimal price);
+    }
+}
---
+++ BookStore.Core/Abstractions/IBooksService.cs
@@ -0,0 +1,12 @@
+﻿using BookStore.CoreDomain.Models;
+
+namespace BookStore.CoreDomain.Abstractions
+{
+    public interface IBooksService
+    {
+        Task<Guid> CreateBook(Book book);
+        Task<Guid> Delete(Guid id);
+        Task<List<Book>> GetAllBooks();
+        Task<Guid> UpdateBook(Guid id, string title, string description, decimal price);
+    }
+}
